d787decfec85e3dd9eecc2e5ff46b4d455f783dd
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2020-09-01 22:03:12 -0300

    add script to disable lock

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-
 programs_to_remove() {
     ./programs_to_remove.sh
 }
 
+disable_locks() {
+    sudo rm /var/lib/apt/lists/lock
+    sudo rm /var/lib/dpkg/lock
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm /var/cache/apt/archives/lock
+}
+
+
+
+disable_locks
 
 programs_to_remove
