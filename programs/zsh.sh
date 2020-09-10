14c6f0be64ad14b52d8e7193fb60269cc1996dae
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2020-09-09 21:02:35 -0300

    add script to install and configure zsh

---
+++ programs/zsh.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt install -y zsh
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+process_id=$!
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+
+wait $process_id
+
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+sed -i 's/robbyrussell/spaceship/g' ~/.zshrc
+
+cat <<EOT >> ~/.zshrc
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+EOT
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
+# sed "/cdef/aline1\nline2\nline3\nline4" input.txt
+# sed "/### End of ZInit's installer chunk/azinit light zdharma/fast-syntax-highlighting\nzinit light zsh-users/zsh-autosuggestions\nzinit light zsh-users/zsh-completions" ~/.zshrc
+
+sed "/^### End of Zinit's installer chunk$/r"<(
+    echo "zinit light zdharma/fast-syntax-highlighting"
+    echo "zinit light zsh-users/zsh-autosuggestions"
+    echo "zinit light zsh-users/zsh-completions"
+    echo "\n"
+) -i -- ~/.zshrc
