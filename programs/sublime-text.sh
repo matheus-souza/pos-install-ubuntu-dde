42e5e4e36f06579520de8788ffb26dde800ef80e
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2020-09-14 20:22:21 -0300

    fix install sublime and plugins

--- programs/sublime-text.sh
+++ programs/sublime-text.sh
@@ -12,17 +12,10 @@ sudo apt-get install sublime-text -y
 
 wget -P ~/.config/sublime-text-3/Installed\ Packages/ https://packagecontrol.io/Package%20Control.sublime-package
 
-#subl --command close
-subl --command start
-
-sleep 1m
-
-subl --command close
-
-#~/.config/sublime-text-3/Installed Packages
-
 wget -P ~/.config/sublime-text-3/Packages/User/ https://gist.githubusercontent.com/matheus-souza/4881c35588d50f99ebe5651958003f66/raw/304bea15f1c499c4d6bd51116c346d81289175ea/Package%2520Control.sublime-settings
+
+wget -P ~/Downloads/ https://gist.githubusercontent.com/matheus-souza/4881c35588d50f99ebe5651958003f66/raw/304bea15f1c499c4d6bd51116c346d81289175ea/Package%2520Control.sublime-settings
 
 wget -P ~/.config/sublime-text-3/Packages/User/ https://gist.githubusercontent.com/matheus-souza/0641e9fcfb84cccab8c20148224729b9/raw/b3becf6621df144687e0e8f321f4ae832fba7d8b/Preferences.sublime-settings
 
-subl --command start
+subl --command start &
